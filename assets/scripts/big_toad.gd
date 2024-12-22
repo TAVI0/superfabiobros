40d6579e52a907b7b33046595e5393f64044245e
TAVI0 <marcos_tavio@outlook.com>
2024-12-22 05:09:34 -0300

    agregado patrullaje de los enemigos

---
+++ assets/scripts/big_toad.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+var SPEED =  -25.0
+var facing_right = false
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D   
+func _ready():
+	add_to_group("Enemy")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if !$RayCast2D.is_colliding() && is_on_floor():
+		flip()
+	if $RayCast2D2.is_colliding() && is_on_floor():
+		flip()
+	# Set the horizontal velocity to a constant negative value to move left.
+	velocity.x = SPEED
+	
+	update_animation()
+	move_and_slide()
+
+func flip():
+	facing_right = !facing_right
+	scale.x = abs(scale.x) * -1
+	if facing_right:
+		SPEED = abs(SPEED)
+	else:
+		SPEED = abs(SPEED) * -1
+	
+
+func update_animation():
+	animated_sprite_2d.play("hop")
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		queue_free()
