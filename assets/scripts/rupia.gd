768eb8a84451844ab54b3fbf0dc153aefeb4c1de
TAVI0 <marcos_tavio@outlook.com>
2024-12-24 11:40:53 -0300

    sound fx y agarrado de rupias

--- assets/scripts/rupia.gd
+++ assets/scripts/rupia.gd
@@ -1,16 +1,10 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+var is_collected = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		queue_free()
+	if body.is_in_group("Player") and not is_collected:
+		is_collected= true
+		$Area2D/AudioStreamPlayer2D.play()
+		GLOBAL.total_coins+=1
+		hide()
