16b881b5d454c1780b47a531ac695245994fd652
TAVI0 <marcos_tavio@outlook.com>
2025-01-06 21:27:12 -0300

    prueba de configuracion de layers y masks

--- assets/scripts/beer.gd
+++ assets/scripts/beer.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var speed = 30
 var direction = -1
+var facing_right = true
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
@@ -12,6 +13,17 @@ func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	velocity.x = speed * direction
 	
+	if $RayCast2D.is_colliding() && is_on_floor():
+		flip()
+
+func flip():
+	facing_right = !facing_right
+	scale.x = abs(scale.x) * -1
+	if facing_right:
+		speed = abs(speed)
+	else:
+		speed = abs(speed) * -1
+
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		body.become_big()
